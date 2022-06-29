47d08918918e2cff3925cfcbcaa27f07629808d8
kavya sri potthuri <99235958+pkavyasri15@users.noreply.github.com>
2022-06-29 15:03:18 +0530

    callback function in js

---
+++ callback.js
@@ -0,0 +1,13 @@
+function verifylogin(user,pwd,callback){
+    if(pwd=="pwd1")
+        callback(null,"You logged in successfully")
+    else    
+        callback("your login credentials are incorrect",null)
+}
+verifylogin("kav","pwd2",function(err,res){
+    if(err)
+        console.log(err)
+    else
+        console.log(res)
+})
+
